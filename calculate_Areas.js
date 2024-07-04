b05a6ace7bd69616cde03eed825626edd6277b26
BeatrizD-34 <dbeatriz888@gmail.com>
2024-07-03 23:36:13 -0400

    new files to Java

--- calculate_Areas.js
+++ calculate_Areas.js
@@ -6,6 +6,17 @@ function calculateArea() {
     width = parseFloat(document.getElementById('width').value);
     
     let area = length * width;
-}
 
 document.getElementById('result').innerText = ' The area of the rectangle is: ${area}';
+}
+
+function calculateTotal(){
+    const grocery1 = parseFloat(document.getElementById('grocery1').value) ;
+    const grocery2 = parseFloat(document.getElementById('grocery2').value) ;
+    const grocery3 = parseFloat(document.getElementById('grocery3').value) ;
+
+    const totalAmount = groceryTracker(grocery1, grocery2, grocery3);
+
+    document.getElementById('totalAmount').textContent = 'totalAmount';
+}
+
