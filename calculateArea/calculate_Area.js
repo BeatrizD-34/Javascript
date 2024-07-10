561040a8bac92524382fd9548488da962fa45d2b
BeatrizD-34 <dbeatriz888@gmail.com>
2024-07-10 00:27:22 -0400

    new update

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -11,5 +11,18 @@ document.getElementById('result').innerText = `The area of the rectangle is ${ar
 
 }
 
+function groceryTracker(amount1, amount2, amount3) {
+const totalAmount = amount1 + amount2 + amount3;
+return totalAmount;
+}
 
+function calculateTotal() {
+    const grocery1 = parseFloat(document.getElementById('grocery1').value) || 0;
+    const grocery2 = parseFloat(document.getElementById('grocery2').value) || 0;
+    const grocery3 = parseFloat(document.getElementById('grocery3').value) || 0;
 
+    const totalAmount = groceryTracker(grocery1, grocery2, grocery3);
+
+    document.getElementById('totalAmount').textContent = totalAmount;
+    
+}
